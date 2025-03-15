d3c602bee687147639093f4bc930137cc14177a6
Oksana <oksanatrokhina76@gmail.com>
2025-03-15 21:46:32 +0200

    created First Selenium Project.1

--- src/test/java/com/telran/qa46/Homework1.java
+++ src/test/java/com/telran/qa46/Homework1.java
@@ -1,4 +1,36 @@
 package com.telran.qa46;
 
-public class Homework1 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework1{
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.navigate().to("https://ticket-service-69443.firebaseapp.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://www.google.com/");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+    @Test
+    public void openTicketService(){
+        System.out.println("Ticket-service was opened");
+    }
+    @AfterMethod(enabled = true)
+    public void tearDown(){
+        driver.quit();
+    }
+
 }
